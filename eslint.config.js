55ce456794b2a54a974cdc0ede492e1fd274171a
mizdra <pp.mizdra@gmail.com>
2025-04-12 20:23:22 +0900

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'eslint/config';
+import css from '@eslint/css';
+import cssModulesKit from '@css-modules-kit/eslint-plugin';
+
+export default defineConfig([
+  {
+    files: ['**/*.css'],
+    language: 'css/css',
+    languageOptions: {
+      tolerant: true,
+    },
+    extends: [css.configs.recommended, cssModulesKit.configs.recommended],
+  },
+]);
